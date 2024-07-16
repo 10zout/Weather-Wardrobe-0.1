931525d680b13d0e4845fd3956900d6622786618
10zout <tenzinthunsur@gmail.com>
2024-07-16 01:14:57 -0400

    backend work

--- WeatherWardrobe/backend/server.js
+++ WeatherWardrobe/backend/server.js
@@ -5,18 +5,16 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const app = express();
-
 app.use(express.json());
 
 const authRoutes = require('./routes/auth');
+const outfitHistoryRoutes = require('./routes/outfitHistory');
 app.use('/api/auth', authRoutes);
+app.use('/api/outfitHistory', outfitHistoryRoutes);
 
-mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-})
+mongoose.connect(process.env.MONGO_URI)
 .then(() => console.log('MongoDB connected'))
 .catch(err => console.log(err));
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5002;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
